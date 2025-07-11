99f580c3225630b9d7ee2986da0346dccff8ff6f
QuangHung <quanghungdang253@gmail.com>
2025-07-12 06:02:01 +0700

    feats: add custom hook to get the course list

--- src/hooks/useFetchCourses.js
+++ src/hooks/useFetchCourses.js
@@ -2,4 +2,41 @@ import { useEffect,useState } from "react";
 
 import axiosClient from "../api/axios-client";
 
-const url = "/data/"
+
+const useFetchCourses = (url = "/data/products.json") => {
+     const [courses, setCourses] = useState([]);
+     const [error,setError] = useState(null);
+    const [loading,setLoading] = useState(true)
+    useEffect(() => {
+            let cancel = false;
+            const handleFetchData = async () => {
+                    setLoading(true);
+                    setError(null);
+                    try {
+                        const getData = await axiosClient.get(url);
+                        if(!cancel){
+                            setCourses(getData);
+                        }
+                    }catch (error) {
+                    if (!cancel) {
+                            setError(error);
+                    }
+                    } finally {
+                     if (!cancel) {
+                             setLoading(false);
+                    }
+                }
+            }
+                handleFetchData();
+            return () => {
+                cancel = true;
+            }
+
+    },[url])
+
+    return {courses,loading, error};
+
+
+}
+
+export default useFetchCourses;
