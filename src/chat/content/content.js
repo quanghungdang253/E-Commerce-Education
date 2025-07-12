25dcd04b9d459922fe7d3f16df1b3e8bcbba7645
QuangHung <quanghungdang253@gmail.com>
2025-07-13 05:47:39 +0700

    update content chatbot

--- src/chat/content/content.js
+++ src/chat/content/content.js
@@ -7,8 +7,9 @@ let contentWelcome = [
 
 
 let content = {
-            "Tôi muốn học tiếng anh với người mỹ": "Xin chào bạn bên trung tâm có đào tạo học tiếng anh với người mỹ",
-            "bên bạn có các dòng điện thoại nào vậy": "Dạ bên cửa hàng hiện có các dòng smartphone hiện đại như Xiamio , Iphone , Vivo , Oppo. ngoài ra còn rất nhiều loại sản phẩm hiện đại khác ạ ",
+            "hello": "Xin chào/mình là trợ lý ảo của trung tâm rất vui được hỗ trợ bạn",
+            "Tôi muốn học tiếng anh với người mỹ": "Xin chào bạn bên trung tâm có đào tạo học tiếng anh với người mỹ với các giảng viên có nhiều năm kinh nghiêm trong nghề.",
+         
             
           
     
